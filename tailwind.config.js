34cb235f75d3f858810fcb4e68ce05bd6612366a
Matias Negrelli <97616@sistemas.frc.utn.edu.ar>
2024-07-28 22:32:19 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // Todos los archivos HTML y JS en src y subcarpetas
+    './*.{html,js}', // Archivos HTML y JS en el directorio raíz
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
